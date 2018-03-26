38e89796b4d1c76d719bb4acde1a1fe9fba23482
Chin Godawita <chinthaka.godawita@lendingworks.co.uk>
2018-03-26 15:30:40 +0100

    Added base nginx/php containers.

---
+++ build.rb
@@ -0,0 +1,84 @@
+#!/usr/bin/env ruby
+
+TAG_PREFIX = "lendingworks"
+PHP_VERSION = "7.2"
+NGINX_VERSION = "1.13"
+ALPINE_VERSION = "3.7"
+
+def get_timestamp
+  Time.now.strftime("%d/%m/%Y %H:%M:%S")
+end
+
+def system! (cmd:, env: {}, ignore_exit: false)
+  green_code = 32
+  puts "\e[#{green_code}m[#{get_timestamp}] #{cmd}\e[0m"
+  system(env, cmd)
+
+  # Non-zero exit on failure.
+  exit $?.exitstatus unless $?.success? or ignore_exit
+end
+
+def get_build_cmd (tag:, dir:, args: {}, pull: true)
+  build_args = args.empty? ? "" : args.map{|k,v| " --build-arg \"#{k}=#{v}\""}.join
+  full_tag = "#{TAG_PREFIX}/#{tag}"
+  pull_arg = pull ? " --pull" : ""
+  "docker build#{pull_arg} --force-rm#{build_args} -t #{full_tag} #{dir}"
+end
+
+def build(tag:, dir:, args: {}, pull: true)
+  build_cmd = get_build_cmd(
+    tag: tag,
+    dir: dir,
+    args: args,
+    pull: pull,
+  )
+  system!(cmd: build_cmd)
+end
+
+startTime = Time.now
+
+# Build base nginx container.
+["nginx:#{NGINX_VERSION}", "nginx:latest"].each do |tag|
+  build(
+    tag: tag,
+    dir: "nginx/base",
+    args: {
+      "NGINX_VERSION" => NGINX_VERSION
+    }
+  )
+end
+
+def do_php_build(dir:, php_tag:, pull: true)
+  ["fpm", "cli"].each do |type|
+    ["#{php_tag}:#{PHP_VERSION}-#{type}", "#{php_tag}:latest-#{type}"].each do |tag|
+      build(
+        tag: tag,
+        dir: dir,
+        args: {
+          "ALPINE_VERSION" => ALPINE_VERSION,
+          "PHP_VERSION" => PHP_VERSION,
+          "PHP_TYPE" => type,
+        },
+        pull: pull
+      )
+    end
+  end
+end
+
+# Build base PHP containers.
+do_php_build(
+  dir: "php/base",
+  php_tag: "php",
+)
+
+# Build development PHP containers.
+do_php_build(
+  dir: "php/dev",
+  php_tag: "php-dev",
+  pull: false,
+)
+
+finishTime = Time.now
+diffTime = finishTime - startTime
+
+puts "\e[1;32m[#{get_timestamp}] All builds complete in #{diffTime.to_s} secs!\e[0m"
