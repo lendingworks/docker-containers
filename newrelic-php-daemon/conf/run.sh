0032ee2ae23b6070d4b4e8e0bf074ce0ee868094
Chin Godawita <chinthaka.godawita@lendingworks.co.uk>
2018-09-26 15:40:49 +0100

    Simplified NewRelic daemon startup.

--- newrelic-php-daemon/conf/run.sh
+++ newrelic-php-daemon/conf/run.sh
@@ -5,18 +5,4 @@ set -e
 # Inspired by https://github.com/hipages/docker-newrelic-php-daemon/blob/master/build/docker-entrypoint
 socat TCP-LISTEN:${NEWRELIC_CONTAINER_PORT},reuseaddr,fork,su=nobody TCP:127.0.0.1:${NEWRELIC_AGENT_PORT} &
 
-/usr/bin/newrelic-daemon -c /etc/newrelic/newrelic.cfg -f &
-CHILD_PID="$!"
-
-if [[ "${IS_KUBERNETES}" == "1" ]]; then
-  (while true; do if [[ -f /opt/signals/SIGTERM ]]; then kill $CHILD_PID; fi; sleep 1; done) &
-fi
-
-wait $CHILD_PID
-EXIT_STATUS=$?
-
-if [[ "${IS_KUBERNETES}" == "1" && -f /opt/signals/SIGTERM ]]; then
-  exit ${EXIT_STATUS}
-elif [[ "${IS_KUBERNETES}" == "0" ]]; then
-  exit ${EXIT_STATUS}
-fi
+exec /usr/bin/newrelic-daemon -c /etc/newrelic/newrelic.cfg -f
